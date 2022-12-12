d76c90783760d6763019a1e03d088405c88312ae
MohamedMahmoud9797 <105246500+MohamedMahmoud9797@users.noreply.github.com>
2022-12-12 18:54:44 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+  flatpickr("#inputCheckOut", {
+    dateFormat: "Y-m-d H:i",
+    enableTime: true,
+
+})
+flatpickr("#inputCheckIn",{
+    dateFormat: "Y-m-d H:i",
+    enableTime: true,
+
+});
+
+$('.level-slider').slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToSlide: 3,
+  });
+
+
